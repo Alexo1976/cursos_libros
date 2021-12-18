e7017848dc6e3f0e0f87b73bb6a9017e23988e29
Enmanuelle Acuña <emanuelacu@outlook.com>
2021-12-18 00:57:54 -0600

    Added code from chapter 1: Hoola mundo, CAritmetica (Ejercicio propuesto)

---
+++ 2_libro_Java_2_curso_de_programacion/Capítulo_1/Ejercicios_propuestos/CAritmetica.java
@@ -0,0 +1,30 @@
+class CAritmetica
+{
+    /*
+    * Operaciones aritéticas
+    */
+	public static void main (String[] args)
+	{
+		int dato1, dato2, dato3, resultado;
+
+		dato1 = 10;
+		dato2 = 20;
+		dato3 = 30;
+
+		//Suma
+		resultado = resultado = dato1 + dato2 + dato3;
+		System.out.println(dato1 + " + " + dato2 + " + " + dato3 + " = " + resultado);
+
+		//Resta
+		resultado = dato1 - dato2 - dato3;
+		System.out.println(dato1 + " - " + dato2 + " - " + dato3 + " = " + resultado);
+
+		//Producto
+		resultado = dato1 * dato2 * dato3;
+		System.out.println(dato1 + " * " + dato2 + " + " + dato3 + " = " + resultado);
+
+		//Cociente
+		resultado = dato1 / dato2 / dato3;
+		System.out.println(dato1 + " / " + dato2 + " / " + dato3 + " = " + resultado);
+	}
+}
